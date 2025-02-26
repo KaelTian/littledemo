443b1be555a0a000645be26bd7c1b1fa23623821
Kael.Tian <Kael_Tian@163.com>
2025-02-26 16:56:40 +0800

    support custom protocol for TCP client and server

--- CustomProtocolClient/Program.cs
+++ CustomProtocolClient/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Text;
+
+const string ServerAddress = "127.0.0.1";
+const int Port = 12345;
+
+var client = new TcpClient(ServerAddress, Port);
+var stream = client.GetStream();
+
+// 构建要发送的数据
+string requestMessage = "Hello from client!";
+byte[] data = Encoding.UTF8.GetBytes(requestMessage);
+byte[] lengthPrefix = BitConverter.GetBytes(data.Length);
+
+// 发送请求数据：前4个字节表示数据长度，接着是数据内容
+stream.Write(lengthPrefix, 0, lengthPrefix.Length);
+stream.Write(data, 0, data.Length);
+Console.WriteLine($"Sent message to server: {requestMessage}");
+
+// 读取服务器响应的消息长度
+byte[] lengthBuffer = new byte[4];
+stream.Read(lengthBuffer, 0, 4);
+int responseLength = BitConverter.ToInt32(lengthBuffer, 0);
+Console.WriteLine($"Received response data length: {responseLength}");
+
+// 读取服务器响应的数据内容
+byte[] responseData = new byte[responseLength];
+stream.Read(responseData, 0, responseLength);
+string responseMessage = Encoding.UTF8.GetString(responseData);
+Console.WriteLine($"Received response from server: {responseMessage}");
+
+client.Close();
--- CustomProtocolServer/Program.cs
+++ CustomProtocolServer/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+const int Port = 12345;
+
+
+var listener = new TcpListener(IPAddress.Any, Port);
+listener.Start();
+Console.WriteLine("Server started...");
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine("Client connected...");
+
+    var stream = client.GetStream();
+
+    // 读取客户端发送的消息长度（前4个字节）
+    byte[] lengthBuffer = new byte[4];
+    stream.Read(lengthBuffer, 0, 4);
+    int dataLength = BitConverter.ToInt32(lengthBuffer, 0);
+    Console.WriteLine($"Received data length: {dataLength}");
+
+    // 读取客户端发送的数据内容
+    byte[] dataBuffer = new byte[dataLength];
+    stream.Read(dataBuffer, 0, dataLength);
+    string receivedMessage = Encoding.UTF8.GetString(dataBuffer);
+    Console.WriteLine($"Received message: {receivedMessage}");
+
+    // 假设根据接收到的数据进行某些处理（这里我们简单地回复一个固定消息）
+    string responseMessage = "Message processed successfully!";
+    byte[] responseData = Encoding.UTF8.GetBytes(responseMessage);
+    byte[] responseLength = BitConverter.GetBytes(responseData.Length);
+
+    // 发送响应数据：先发送长度，再发送数据
+    stream.Write(responseLength, 0, responseLength.Length);
+    stream.Write(responseData, 0, responseData.Length);
+    Console.WriteLine($"Sent response to client: {responseMessage}");
+
+    client.Close();
+}
